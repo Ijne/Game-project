0ada5d69cb40f272282b32db7be1f887a63710a6
Никита <nikitka_smirnov_2007@list.ru>
2022-01-25 20:42:16 +0300

    Add requirements

--- The World/polygon.py
+++
@@ -1,9 +0,0 @@
-top = (hero.position[0] % 10,  hero.position[1] % 10)
-bottom = (top[0] + 20,  top[1] + 20)
-veiw_field = []
-for x in range(top[x], bottom[x] + 1):
-    column = []
-    for y in range(top[y], bottom[y] + 1):
-        column.append(board.field[x][y])
-    view_field.apppend(column)
-
